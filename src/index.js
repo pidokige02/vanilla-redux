b92cccd3feaf87f67381dcfa7f2c3877ee14b515
pidokige02 <pidokige0204@gmail.com>
2023-06-24 18:23:36 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// below is a function
-// count = 0 means if count is undefined, count defaul is 0
-const countModifier = (count = 0) => {
-  return count;
-  // return "Hello" // getState () return "Hello".
-}
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
 
 // put reducer as param, when cretaStore is called.
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
